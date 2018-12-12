a02f507213dd9746ed103f825cb1f8c0be97d3f4
Chidiebere <chidex5@gmail.com>
2018-12-12 21:06:56 +0100

    fix: fix bug with not converting from one currency to another

--- js/final.js
+++ js/final.js
@@ -1,32 +1,32 @@
 
-	// //Registering the Service Worker
+// //Registering the Service Worker
 
-	if ('serviceWorker' in navigator) {
-		window.addEventListener('load', () => {
-			navigator.serviceWorker.register('sw.js').then(registration => {
-				
-			}).catch(error => {
-				
-			});
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker.register('sw.js').then(registration => {
+
+		}).catch(error => {
+
 		});
-	}
-	
-	const dbPromise = idb.open('currency-rates', 1, upgradeDB => {
-		switch (upgradeDB.oldVersion) {
-			case 0:
-			upgradeDB.createObjectStore('objs', {keyPath: 'id'});
-		}
 	});
+}
 
-	fetch("https://free.currencyconverterapi.com/api/v5/currencies")
-	.then((response) => response.json())
-	.then((data) => {
-		
+const dbPromise = idb.open('currency-rates', 1, upgradeDB => {
+	switch (upgradeDB.oldVersion) {
+		case 0:
+			upgradeDB.createObjectStore('objs', { keyPath: 'id' });
+	}
+});
+
+fetch("https://free.currencyconverterapi.com/api/v5/currencies")
+	.then(response => response.json())
+	.then(data => {
+
 		for (const key in data) {
-		  return data[key];
+			return data[key];
 		}
 	})
-	.then((datakey) => {
+	.then(datakey => {
 		for (const key2 in datakey) {
 			const id = datakey[key2].id;
 			const CURRENCY_NAME = datakey[key2].currencyName;
@@ -34,46 +34,44 @@
 		}
 	})
 	.catch((error) => {
-		
+
 	})
-	
-	
-
-	const convertCurrency = (isRateFound) => 
-		{ 
-		
-		const homeCurrency = document.getElementById("homeCurrency").value;
-		const travelCurrency = document.getElementById("travelCurrency").value;
-		const query = `${homeCurrency}_${travelCurrency}`;
 
 
-		fetch('https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra')
-			.then(response => {
-				return response.json();
+
+const convertCurrency = (isRateFound) => {
+
+	const homeCurrency = document.getElementById("homeCurrency").value;
+	const travelCurrency = document.getElementById("travelCurrency").value;
+	const query = `${homeCurrency}_${travelCurrency}`;
+
+
+	fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`)
+		.then(response => {
+			return response.json();
+		})
+		.then(data => {
+			dbPromise.then(db => {
+				if (!db) return;
+				const tx = db.transaction('objs', 'readwrite');
+				const store = tx.objectStore('objs');
+
+				store.put({ id: query, rates: data });
+				return tx.complete;
 			})
-			.then(data => {
-				dbPromise.then(db => {
-					if(!db) return;
-					const tx = db.transaction('objs', 'readwrite');
-					const store = tx.objectStore('objs');
-					
-					store.put({id: query, rates: data});
-					return tx.complete;
-				})
-				
-				
-				const oneUnit = data[query];
-				const amt = document.getElementById("homeValue").value;
-				document.getElementById("travelValue").value =  ` ${(oneUnit*amt).toFixed(2)}`;
-				
-			})
-			.catch(() => {
-				if (!isRateFound) {
-					alert('Cannot convert this while offline');
-				}
-						
-			});
-			
-		
-	}
-	
+
+
+			const oneUnit = data[query];
+			const amt = document.getElementById("homeValue").value;
+			document.getElementById("travelValue").value = `${travelCurrency} ${(oneUnit * amt).toFixed(2)}`;
+
+		})
+		.catch(() => {
+			if (!isRateFound) {
+				alert('Cannot convert this while offline');
+			}
+
+		});
+
+
+}
--- sw.js
+++ sw.js
@@ -1,48 +1,48 @@
-const cacheName = 'cache-v2';
+const cacheName = 'cache-v9';
 
-	const filesToCache = [
-		'./index.html',
-		'js/final.js',
-		'js/idb.js',
-		'js/jquery.min.js',
-		'css/style.css',
-		'css/reset.css',
-	
-	];
-	
-	// cache assets
-	self.addEventListener('install', event => {
-	  event.waitUntil(
-        caches.open(cacheName)
-        .then(cache => {
-            return cache.addAll(filesToCache);
-        })
-      );
-	});
-	
-	//delete unused cache
-	self.addEventListener('activate', event => {
-	  event.waitUntil(
-		  caches.keys()
+const filesToCache = [
+	'./index.html',
+	'js/final.js',
+	'js/idb.js',
+	'js/jquery.min.js',
+	'css/style.css',
+	'css/reset.css',
+
+];
+
+// cache assets
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open(cacheName)
+			.then(cache => {
+				return cache.addAll(filesToCache);
+			})
+	);
+});
+
+//delete unused cache
+self.addEventListener('activate', event => {
+	event.waitUntil(
+		caches.keys()
 			.then(keyList => Promise.all(keyList.map(thisCacheName => {
-            if (thisCacheName !== cacheName){
-                return caches.delete(thisCacheName);        
-            }
-        })))
-		);
-	  return self.clients.claim();
-	});
+				if (thisCacheName !== cacheName) {
+					return caches.delete(thisCacheName);
+				}
+			})))
+	);
+	return self.clients.claim();
+});
 
-	//fetch cache 
-	self.addEventListener('fetch', event => {
-	  event.respondWith(caches.match(event.request)
+//fetch cache 
+self.addEventListener('fetch', event => {
+	event.respondWith(caches.match(event.request)
 		.then(response => response || fetch(event.request)
-      .then(response => caches.open(cacheName)
-      .then(cache => {
-        cache.put(event.request, response.clone());
-        return response;
-      })).catch(event => {
-  
-    }))
-	 );
-	});
+			.then(response => caches.open(cacheName)
+				.then(cache => {
+					cache.put(event.request, response.clone());
+					return response;
+				})).catch(event => {
+
+				}))
+	);
+});
