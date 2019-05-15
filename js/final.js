76088c7cce19932e900b6c4607b31a57fcd2134f
Chidiebere <chidex5@gmail.com>
2019-05-15 08:08:54 +0100

    chore: Update API KEY

--- js/final.js
+++ js/final.js
@@ -1,77 +1,75 @@
+// API KEY
 
-// //Registering the Service Worker
+const mykey = config.API_KEY;
 
-if ('serviceWorker' in navigator) {
-	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('sw.js').then(registration => {
+//Registering the Service Worker
 
-		}).catch(error => {
-
-		});
-	});
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("sw.js")
+      .then(registration => {})
+      .catch(error => {});
+  });
 }
 
-const dbPromise = idb.open('currency-rates', 1, upgradeDB => {
-	switch (upgradeDB.oldVersion) {
-		case 0:
-			upgradeDB.createObjectStore('objs', { keyPath: 'id' });
-	}
+const dbPromise = idb.open("currency-rates", 1, upgradeDB => {
+  switch (upgradeDB.oldVersion) {
+    case 0:
+      upgradeDB.createObjectStore("objs", { keyPath: "id" });
+  }
 });
 
-fetch("https://free.currencyconverterapi.com/api/v5/currencies")
-	.then(response => response.json())
-	.then(data => {
+fetch(`https://free.currconv.com/api/v7/currencies?apiKey=${mykey}`)
+  .then(response => response.json())
+  .then(data => {
+    for (const key in data) {
+      return data[key];
+    }
+  })
+  .then(datakey => {
+    for (const key2 in datakey) {
+      const id = datakey[key2].id;
+      const CURRENCY_NAME = datakey[key2].currencyName;
+      $("#homeCurrency, #travelCurrency").append(
+        $("<option>")
+          .text(`${CURRENCY_NAME}  ${id}`)
+          .attr("value", id)
+      );
+    }
+  })
+  .catch(error => {});
 
-		for (const key in data) {
-			return data[key];
-		}
-	})
-	.then(datakey => {
-		for (const key2 in datakey) {
-			const id = datakey[key2].id;
-			const CURRENCY_NAME = datakey[key2].currencyName;
-			$('#homeCurrency, #travelCurrency').append($('<option>').text(`${CURRENCY_NAME}  ${id}`).attr('value', id));
-		}
-	})
-	.catch((error) => {
+const convertCurrency = isRateFound => {
+  const homeCurrency = document.getElementById("homeCurrency").value;
+  const travelCurrency = document.getElementById("travelCurrency").value;
+  const query = `${homeCurrency}_${travelCurrency}`;
 
-	})
+  fetch(
+    `https://free.currconv.com/api/v7/convert?q=${query}&compact=ultra&apiKey=${mykey}`
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      dbPromise.then(db => {
+        if (!db) return;
+        const tx = db.transaction("objs", "readwrite");
+        const store = tx.objectStore("objs");
 
+        store.put({ id: query, rates: data });
+        return tx.complete;
+      });
 
-
-const convertCurrency = (isRateFound) => {
-
-	const homeCurrency = document.getElementById("homeCurrency").value;
-	const travelCurrency = document.getElementById("travelCurrency").value;
-	const query = `${homeCurrency}_${travelCurrency}`;
-
-
-	fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`)
-		.then(response => {
-			return response.json();
-		})
-		.then(data => {
-			dbPromise.then(db => {
-				if (!db) return;
-				const tx = db.transaction('objs', 'readwrite');
-				const store = tx.objectStore('objs');
-
-				store.put({ id: query, rates: data });
-				return tx.complete;
-			})
-
-
-			const oneUnit = data[query];
-			const amt = document.getElementById("homeValue").value;
-			document.getElementById("travelValue").value = `${travelCurrency} ${(oneUnit * amt).toFixed(2)}`;
-
-		})
-		.catch(() => {
-			if (!isRateFound) {
-				alert('Cannot convert this while offline');
-			}
-
-		});
-
-
-}
+      const oneUnit = data[query];
+      const amt = document.getElementById("homeValue").value;
+      document.getElementById("travelValue").value = `${travelCurrency} ${(
+        oneUnit * amt
+      ).toFixed(2)}`;
+    })
+    .catch(() => {
+      if (!isRateFound) {
+        alert("Cannot convert this while offline");
+      }
+    });
+};
--- sw.js
+++ sw.js
@@ -1,48 +1,54 @@
-const cacheName = 'cache-v9';
+const cacheName = "cache-v10";
 
 const filesToCache = [
-	'./index.html',
-	'js/final.js',
-	'js/idb.js',
-	'js/jquery.min.js',
-	'css/style.css',
-	'css/reset.css',
-
+  "./index.html",
+  "js/final.js",
+  "js/idb.js",
+  "js/config.js",
+  "js/jquery.min.js",
+  "css/style.css",
+  "css/reset.css"
 ];
 
 // cache assets
-self.addEventListener('install', event => {
-	event.waitUntil(
-		caches.open(cacheName)
-			.then(cache => {
-				return cache.addAll(filesToCache);
-			})
-	);
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
 //delete unused cache
-self.addEventListener('activate', event => {
-	event.waitUntil(
-		caches.keys()
-			.then(keyList => Promise.all(keyList.map(thisCacheName => {
-				if (thisCacheName !== cacheName) {
-					return caches.delete(thisCacheName);
-				}
-			})))
-	);
-	return self.clients.claim();
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(thisCacheName => {
+          if (thisCacheName !== cacheName) {
+            return caches.delete(thisCacheName);
+          }
+        })
+      )
+    )
+  );
+  return self.clients.claim();
 });
 
-//fetch cache 
-self.addEventListener('fetch', event => {
-	event.respondWith(caches.match(event.request)
-		.then(response => response || fetch(event.request)
-			.then(response => caches.open(cacheName)
-				.then(cache => {
-					cache.put(event.request, response.clone());
-					return response;
-				})).catch(event => {
-
-				}))
-	);
+//fetch cache
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(
+      response =>
+        response ||
+        fetch(event.request)
+          .then(response =>
+            caches.open(cacheName).then(cache => {
+              cache.put(event.request, response.clone());
+              return response;
+            })
+          )
+          .catch(event => {})
+    )
+  );
 });
